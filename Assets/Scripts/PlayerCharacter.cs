6bc8a4dc809134bf42d5b9aacba66f9a4f19b46b
Marcello Vaz <marcello.vaz@sciensa.com>
2018-07-28 00:53:07 -0300

    feat: first commit

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+
+	public float speed;
+	public GameObject startingRail;
+
+	private Rail currentRail;
+	private GameObject nextWayPoint;
+
+	void Awake() {
+		this.currentRail = this.startingRail.GetComponent<Rail>();
+	}
+
+	void Start() {
+		NextWaypoint();
+		print(nextWayPoint.transform.position);
+	}
+
+	void Update () {
+		Move();
+	}
+
+	void Move() {
+		if (transform.position == nextWayPoint.transform.position) NextWaypoint();
+		transform.position = Vector3.MoveTowards(transform.position, nextWayPoint.transform.position, Time.deltaTime * speed);
+	}
+
+	void NextWaypoint() {
+		nextWayPoint = currentRail.GetNextWaypoint();
+	}
+}
---
+++ Assets/Scripts/Rail.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rail : MonoBehaviour {
+
+	private List<GameObject> waypoints;
+	private IEnumerator<GameObject> waypointEnum;
+
+	void Awake() {
+		var waypointsTransform = new List<Transform>(GetComponentsInChildren<Transform>());
+		this.waypoints = new List<GameObject>();
+		waypointsTransform.ForEach(e => waypoints.Add(e.gameObject));
+	}
+
+	void Start () {
+		waypointEnum = waypoints.GetEnumerator();
+		waypointEnum.MoveNext();
+	}
+
+	public GameObject GetNextWaypoint() {
+		waypointEnum.MoveNext();
+		return waypointEnum.Current;
+	}
+}
