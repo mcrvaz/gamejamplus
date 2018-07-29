4bd921e7b731c0f019ba562c4704fd1b2b60503b
Marcello Vaz <marcello.vaz@sciensa.com>
2018-07-29 04:20:46 -0300

    feat: background

---
+++ Assets/Parallax.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+	public float speed;
+
+	void Update () {
+		transform.Translate(-speed * Time.deltaTime, 0, 0);
+	}
+}
